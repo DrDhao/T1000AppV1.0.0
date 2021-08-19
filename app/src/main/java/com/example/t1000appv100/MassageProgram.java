a7845a46a46d16efc9eeb0dea9fbe5dfff536eb7
DrDhao <dr.rofl@web.de>
2021-08-19 09:44:53 +0200

    Revert "Massageprogramme angefangen, TODO Array füllen bei Wellemassage"
    This reverts commit 59d0335c

--- app/src/main/java/com/example/t1000appv100/MassageProgram.java
+++ app/src/main/java/com/example/t1000appv100/MassageProgram.java
@@ -1,7 +1,7 @@
 package com.example.t1000appv100;
 
 public abstract class MassageProgram {
-    protected MassageProgramHandler handler;
+    private MassageProgramHandler handler;
 
     public MassageProgram(MassageProgramHandler handler){
         this.handler = handler;
@@ -12,75 +12,26 @@ public abstract class MassageProgram {
 
  class WaveMassage extends MassageProgram{
 
-     private final byte[][] valueTable;
-     private int columnCounter;
-     private final int numberOfColumns = 1920;
-
      public WaveMassage(MassageProgramHandler handler) {
          super(handler);
-         byte bigIncrement;
-         byte littleIncrement;
-
-         valueTable = new byte[handler.getMotorCount()][numberOfColumns];
-         for (int i = 0; i < handler.getMotorCount(); i++) {
-             for (int j = 0; j < numberOfColumns; j++) { //TODO füllen von 2d array
-
-             }
-         }
      }
 
      @Override
     public byte[] nextStep() {
-         byte[] column = new byte[super.handler.getMotorCount()];
-         for (int i = 0; i < super.handler.getMotorCount(); i++) {
-             column[i] = valueTable[i][columnCounter];
-         }
-         if (columnCounter<numberOfColumns){
-             columnCounter++;
-         } else {
-             resetColumnCounter();
-         }
-         return column;
+
+
+         return new byte[0];
     }
-     public void resetColumnCounter(){
-         columnCounter = 0;
-     }
 }
 
 class FullPowerMassage extends MassageProgram{
 
-    private final byte[][] valueTable;
-    private int columnCounter;
-    private final int numberOfColumns = 1920;
-
     public FullPowerMassage(MassageProgramHandler handler) {
         super(handler);
-        byte bigIncrement = 127;
-
-        valueTable = new byte[handler.getMotorCount()][numberOfColumns];
-        for (int i = 0; i < handler.getMotorCount(); i++) {
-            for (int j = 0; j < numberOfColumns; j++) {
-                valueTable[i][j] = bigIncrement;
-            }
-        }
-        columnCounter = 0;
     }
 
     @Override
     public byte[] nextStep() {
-        byte[] column = new byte[super.handler.getMotorCount()];
-        for (int i = 0; i < super.handler.getMotorCount(); i++) {
-            column[i] = valueTable[i][columnCounter];
-        }
-        if (columnCounter<numberOfColumns){
-            columnCounter++;
-        } else {
-            resetColumnCounter();
-        }
-        return column;
-    }
-
-    public void resetColumnCounter(){
-        columnCounter = 0;
+        return new byte[0];
     }
 }
